5a98bdb0d9143be3b565e110dc8c504cf3a1bb6d
Harry Chen <harrychenca@gmail.com>
2015-07-27 01:59:47 +0800

    OOP + Simplified

--- js/application.js
+++ js/application.js
@@ -1,58 +1,14 @@
+'use strict';
+
 $(document).ready(function(){
+  var Chart = function(){
+    this.dataWeekly = [];
+    this.dataMonthly = [];
+    this.dataQuarterly = [];
+    this.dataYearly = [];
+  };
 
-  // data is an array
-  var dataWeekly = [];
-  var dataMonthly = [];
-  var dataQuarterly = [];
-  var dataYearly = [];
-  var monthlySum = 0;
-  var quarterlySum = 0;
-  var yearlySum = 0;
-  var url = 'https://www.quandl.com/api/v1/datasets/BTS_MM/RETAILGAS.json?trim_start=1995-01-02&trim_end=2012-10-15&auth_token=E6kNzExHjay2DNP8pKvB';
-
-  function calcSMA(dataWeekly, data, sum, days, i){
-    if (i < dataWeekly.length - days){
-      for (var j = 0; j < days; j++){
-        sum += dataWeekly[i+j].y;
-      }
-      data.push({ x: dataWeekly[i].x, y: sum / days })
-      sum = 0;
-    }
-  }
-
-  function getPrice(url){
-    $.ajax({
-      type: 'GET',
-      url: url,
-      dataType: 'JSON',
-      success: function(response){
-        $(response.data).each(function(){
-          var dataPoint = {};
-          dataPoint.y = this[1];
-
-          var date = moment(this[0], "YYYY-MM-DD")
-          dataPoint.x = Date.UTC(date.year(), date.month(), date.date());
-
-          dataWeekly.push(dataPoint);
-        })
-
-        for (var i = 0; i < dataWeekly.length; i++) {   
-            calcSMA(dataWeekly, dataMonthly, monthlySum, 4, i);
-            calcSMA(dataWeekly, dataQuarterly, monthlySum, 13, i);
-            calcSMA(dataWeekly, dataYearly, monthlySum, 52, i);
-        }
-
-        initializeHighChart();
-      },
-      error: function(){
-        alert("cannot connect");
-      }
-    });
-  }
-
-  getPrice(url);
-
-  function initializeHighChart(){
+  Chart.prototype.graph = function(){
     $('#chart').highcharts({
       title: {
         text: 'Historical Gasoline Prices'
@@ -78,21 +34,63 @@ $(document).ready(function(){
       },
       series: [{
         name: 'Weekly',
-        data: dataWeekly.reverse()
+        data: this.dataWeekly.reverse()
       },
       {
         name: 'Monthly',
-        data: dataMonthly.reverse()
+        data: this.dataMonthly.reverse()
       },
       {
         name: 'Quarterly',
-        data: dataQuarterly.reverse()
+        data: this.dataQuarterly.reverse()
       },
       {
         name: 'Yearly',
-        data: dataYearly.reverse()
+        data: this.dataYearly.reverse()
       }]
+    });
+  };
+
+  Chart.prototype.calcSMA = function(dataWeekly, dataStorage, days){
+    var sum = 0;
+
+    for (var i = 0; i < dataWeekly.length; i++) {   
+      if (i <= dataWeekly.length - days){
+        for (var j = 0; j < days; j++){
+          sum += dataWeekly[i+j].y;
+        }
+
+        dataStorage.push({ x: dataWeekly[i].x, y: sum / days })
+        sum = 0;
+      }
+    }
+  };
+
+  Chart.prototype.getPrice = function(url){
+    $.ajax({
+      context: this,
+      type: 'GET',
+      url: url,
+      dataType: 'JSON',
+      success: function(response){
+        response.data.forEach(function(datum){
+          this.dataWeekly.push({ x: new Date(datum[0]), y: datum[1] });
+        }, this);
+
+        this.calcSMA(this.dataWeekly, this.dataMonthly, 4);
+        this.calcSMA(this.dataWeekly, this.dataQuarterly, 13);
+        this.calcSMA(this.dataWeekly, this.dataYearly, 52);
+
+        this.graph();
+      },
+      error: function(){
+        alert("cannot connect");
+      }
     });
   }
 
+  var chart = new Chart();
+
+  var url = 'https://www.quandl.com/api/v1/datasets/BTS_MM/RETAILGAS.json?trim_start=1995-01-02&trim_end=2012-10-15&auth_token=E6kNzExHjay2DNP8pKvB';
+  chart.getPrice(url);
 });
